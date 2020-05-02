5ea5a68666e8e092dd8c6438ff9f8a24fb760e52
uimarshall <uimarshall@gmail.com>
2020-05-03 00:36:31 +0100

    added layout

--- client/src/components/user/SignUp.js
+++ client/src/components/user/SignUp.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { API } from "../../config";
+import Layout from "../layout/Layout";
 
 // ==========================TODO'S==================================
 /** 1. Declare the state variable as 'values'
@@ -13,46 +14,61 @@ const SignUp = () => {
 		email: "",
 		password: "",
 		error: "",
-		success: false
+		success: false,
 	});
 
 	// Anytime there is an event in the form, we hv to grab d change and update the state
 	// handleChange is a higher order fn, which is a fn returning another fn
-	const handleChange = name => e => {
+	const handleChange = (name) => (e) => {
 		setValues({ ...values, error: false, [name]: e.target.value });
 	};
 
 	// Function to post data to Db
-	const signUpUser = user => {
-		fetch(`${API}/signup`, {
+	const signUpUser = (user) => {
+		fetch(`${API}/users/signup`, {
 			method: "POST",
 			headers: {
 				Accept: "application/json",
-				"Content-Type": "application/json"
+				"Content-Type": "application/json",
 			},
 			// Convert the obj sent from handleSubmit to json string
-			body: JSON.stringify(user)
+			// Only JSON str can be sent to the backend
+			body: JSON.stringify(user),
 		})
-			.then(res => {
+			.then((res) => {
 				return res.json();
 			})
-			.catch(err => {
+			.catch((err) => {
 				console.log(err);
 			});
 	};
 
 	const { name, email, password, error, success } = values;
-	const handleSubmit = e => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
 		// Frm d User Model, a user must hv name, email,password,set these fields to what is
 		// coming the state(values) as destructured above
 		// Take the form entries stored in d state and submit(click submit) to backend using 'signUp() function'
 		let newUser = { name: name, email: email, password: password };
-		signUpUser(newUser);
+		// Javascript object being received as newUser
+		signUpUser(newUser).then((data) => {
+			if (data.error) {
+				setValues({ ...values, error: data.error });
+			} else {
+				setValues({
+					...values,
+					name: "",
+					email: "",
+					password: "",
+					error: "",
+					success: true,
+				});
+			}
+		});
 	};
 
 	const signUpForm = () => (
-		<form>
+		<form className="mb-2" onSubmit={handleSubmit}>
 			<div className="form-group">
 				<label className="text-muted" htmlFor="">
 					Name
@@ -86,18 +102,24 @@ const SignUp = () => {
 					value={password}
 				/>
 			</div>
-			<button onSubmit={handleSubmit} type="submit" className="btn btn-primary">
+			<button type="submit" className="btn btn-primary">
 				Submit
 			</button>
 		</form>
 	);
 
 	return (
-		<div className="container col-md-8 offset-md-2">
-			<h3>Signup</h3>
-			{signUpForm()}
-			{JSON.stringify(values)}
-		</div>
+		<Layout
+			className="mb-5"
+			title="Welcome here our esteem customer"
+			description="We are at your service to give you the best experience you can get!">
+			<div className="container col-md-8 offset-md-2">
+				<h3>Signup</h3>
+				{signUpForm()}
+				{JSON.stringify(values)}
+			</div>
+			<div>{API}</div>
+		</Layout>
 	);
 };
 
