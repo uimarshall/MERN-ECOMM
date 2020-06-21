62c28601ecb09e40dd2042dc8783a069facdf6a7
uimarshall <uimarshall@gmail.com>
2020-06-21 19:54:20 +0100

    Adds admin dashboard and conditional rendering

--- client/src/components/user/AdminDashboard.js
+++ client/src/components/user/AdminDashboard.js
@@ -3,24 +3,24 @@ import { Link } from "react-router-dom";
 import Layout from "../layout/Layout";
 import { isAuthenticated } from "../auth";
 
-const Dashboard = () => {
+const AdminDashboard = () => {
 	const {
 		userFound: { _id, name, email, role },
 	} = isAuthenticated();
 
-	const userLinks = () => {
+	const adminLinks = () => {
 		return (
 			<div className="card mb-5">
-				<h3 className="card-header">User info</h3>
+				<h3 className="card-header">Admin Links</h3>
 				<ul class="list-group">
 					<li class="list-group-item">
-						<Link className="nav-link" to="/cart">
-							My Cart
+						<Link className="nav-link" to="/create/category">
+							Create Category
 						</Link>
 					</li>
 					<li class="list-group-item">
-						<Link className="nav-link" to="/profile/update">
-							Update Profile
+						<Link className="nav-link" to="/create/product">
+							Create Product
 						</Link>
 					</li>
 				</ul>
@@ -28,7 +28,7 @@ const Dashboard = () => {
 		);
 	};
 
-	const userInfo = () => (
+	const adminInfo = () => (
 		<div className="card mb-5">
 			<h3 className="card-header">User info</h3>
 			<ul class="list-group">
@@ -41,31 +41,17 @@ const Dashboard = () => {
 		</div>
 	);
 
-	const purchaseHistory = () => {
-		return (
-			<div className="card mb-5">
-				<h3 className="card-header">Purchase history</h3>
-				<ul class="list-group">
-					<li class="list-group-item">history</li>
-					<li class="list-group-item disabled">role</li>
-				</ul>
-			</div>
-		);
-	};
 	return (
 		<Layout
 			title="Dashboard"
-			description={`Hi ${name}!`}
+			description={`Hi ${name}, You're signed in as an Admin!`}
 			className="container-fluid">
 			<div className="row">
-				<div className="col-md-3">{userLinks()}</div>
-				<div className="col-md-9">
-					{userInfo()}
-					{purchaseHistory()}
-				</div>
+				<div className="col-md-3">{adminLinks()}</div>
+				<div className="col-md-9">{adminInfo()}</div>
 			</div>
 		</Layout>
 	);
 };
 
-export default Dashboard;
+export default AdminDashboard;
